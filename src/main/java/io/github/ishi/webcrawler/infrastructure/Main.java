8d6b44e5a41f071c8368093183b13282ee73c96d
Radoslaw Zielinski <kontakt@radekzielinski.pl>
2019-07-15 19:07:56 +0200

    Wire it all together in Main method

--- src/main/java/io/github/ishi/webcrawler/infrastructure/Main.java
+++ src/main/java/io/github/ishi/webcrawler/infrastructure/Main.java
@@ -1,6 +1,21 @@
 package io.github.ishi.webcrawler.infrastructure;
 
+import io.github.ishi.webcrawler.core.WebCrawler;
+import io.github.ishi.webcrawler.core.output.XmlSitemapOutputFormat;
+import io.github.ishi.webcrawler.infrastructure.html.HtmlURIExtractor;
+import io.github.ishi.webcrawler.infrastructure.http.HttpDataProvider;
+
+import java.net.http.HttpClient;
+
 public class Main {
     public static void main(String[] args) {
+
+        HttpClient client = HttpClient.newBuilder()
+                .followRedirects(HttpClient.Redirect.NORMAL)
+                .build();
+
+        HttpDataProvider dataProvider = new HttpDataProvider(client);
+        new WebCrawler(dataProvider, new HtmlURIExtractor(), new XmlSitemapOutputFormat(), System.out::println)
+                .analyze("http://wiprodigital.com");
     }
 }
